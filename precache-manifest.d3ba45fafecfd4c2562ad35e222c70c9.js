48aa104228041d21927aceec9b248f944c94b3b2
James Conry <james.conry@comcast.net>
2019-12-12 20:26:44 -0600

    Updates

---
+++ precache-manifest.d3ba45fafecfd4c2562ad35e222c70c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e8b64153e4052d69d5d7830c15fd0e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d967ca95419da83916f5",
+    "url": "/Clicky-Game/static/css/main.540e4287.chunk.css"
+  },
+  {
+    "revision": "c43780d072d862424898",
+    "url": "/Clicky-Game/static/js/2.10ea4d50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.10ea4d50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d967ca95419da83916f5",
+    "url": "/Clicky-Game/static/js/main.024a0386.chunk.js"
+  },
+  {
+    "revision": "26ee0aba715578d6f77c",
+    "url": "/Clicky-Game/static/js/runtime-main.5d0af5bd.js"
+  }
+]);
